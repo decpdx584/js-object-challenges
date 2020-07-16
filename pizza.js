4ab912442975f03aea6b7246a068d0e9501b2a20
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-15 19:59:34 -0700

    [pizza] Add menu object

---
+++ pizza.js
@@ -0,0 +1,23 @@
+const menu = {
+    hawaiian: {
+            toppings: ["ham","pinapple", "sausage"],
+            price: 14.99
+    },
+    meatLovers: {
+            toppings: ["pepperoni", "sausage", "meatball"],
+            price: 16.99
+    },   
+    veggie:  {
+            toppings: ["bell peppers", "black olives", "onions"],
+            price: 13.99
+    },
+    peppMush: {
+            toppings: ["pepperoni", "mushrooms", "roasted garlic"],
+            price: 15.99
+    },
+    cheeseBurger: {
+            toppings: ["ground beef", "cheddar", "onions"],
+            price: 14.99
+    }
+}
+console.log(menu);
